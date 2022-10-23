40d081c88c2cfee13b30eb0c4b76619d0244cad8
Eric Bruhns <bruhe002@gmail.com>
2022-10-22 18:05:45 -0700

    implemented matrix multiply function

--- functions.cpp
+++ functions.cpp
@@ -24,6 +24,17 @@ void printMatrix(int **a, int length) {
 
 }
 
+int **createEmptyMatrix(int n) {
+    int **newArray2D = new int*[n];
+    for(int i = 0; i < n; i++) {
+        newArray2D[i] = new int[n];
+
+        for(int j = 0; j < n; j++) {
+            newArray2D[i][j] = 0; 
+        }
+    }
+}
+
 int **createMatrix(int n) {
     srand(time(NULL));
     int **tempArray2D = new int*[n];
@@ -45,5 +56,31 @@ void deleteMatrix(int **a, int length) {
     }
 
     delete [] a;
+}
+
+// Brute force matrix multiplication
+void matrixMultiply(int **a, int **b, int **product, int sizeN) {
+    // For each row of matrix A, we must multiply with each column of matrix B
+    
+    for(int rowA = 0; rowA < sizeN; rowA++) {
+        for(int colB = 0; colB < sizeN; colB++) {
+            int total = 0;
+            for(int idx = 0; idx  < sizeN; idx++) {
+                total += (a[rowA][idx] * b[idx][colB]);
+            }
+            product[rowA][colB] = total;
+        }
+    }
+ 
+}
+
+// Regular Divide and conquer algorithm
+int **matixMultiplyDivideAndConquer(int **a, int **b, int sizeN) {
+
+}
+
+// Straussen's Algorithm
+int **matrixMultiplyStraussen(int **a, int **b, int sizeN) {
+
 }
 
--- functions.h
+++ functions.h
@@ -5,13 +5,17 @@
 int getRandomNumber();
 void deleteMatrix(int **a, int length);
 void printMatrix(int **a, int length);
+int **createEmptyMatrix(int n);
 int **createMatrix(int n);
 
-// Brute force matrix multiplication 
+// Brute force matrix multiplication
+void matrixMultiply(int **a, int **b, int **product, int sizeN); 
 
 // Regular Divide and conquer algorithm
+int **matixMultiplyDivideAndConquer(int **a, int **b, int sizeN);
 
 // Straussen's Algorithm
+int **matrixMultiplyStraussen(int **a, int **b, int sizeN);
 
 
 #endif
